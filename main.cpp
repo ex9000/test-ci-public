3d19314a4460ab0c3c701b1920499193fb2076ac
ISA002 <ggeasy1710@gmail.com>
2019-09-30 09:41:56 +0300

    ssc

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <iterator> 
+
+using namespace std;
+
+
+int main () {
+	vector <int> massive;
+	for (int i = 0; i < 5; i++) {
+		int el = 0;
+		cin >> el;
+		massive.push_back(el);
+	}
+	sort(massive.begin(), massive.end());
+	copy(massive.begin(), massive.end(), ostream_iterator<int>(cout, " "));
+	cout << endl;
+  return 0;
+}
+
