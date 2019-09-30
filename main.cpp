f80121f5fe5fde9fad708537ad307405e7b85326
Isignaturka <smanivkk@gmail.com>
2019-09-30 10:18:48 +0300

    уау

--- main.cpp
+++ main.cpp
@@ -3,19 +3,19 @@
 #include <algorithm>
 
 int main() {
-	std::vector<int>input = {};
+    std::vector<int>input = {};
 
 
-	int temp;
-	for (int i = 0; i < 5; i++) {
-		std::cin >> temp;
-		input.push_back(temp);
-	}
+    int temp;
+    for (int i = 0; i < 5; i++) {
+        std::cin >> temp;
+        input.push_back(temp);
+    }
 
-	std::sort(input.begin(), input.end());
-	for (int i : input) {
-		std::cout << i;
-	}
+    std::sort(input.begin(), input.end());
+    for (int i : input) {
+        std::cout << i;
+    }
 
-	return 0;
+    return 0;
 }
