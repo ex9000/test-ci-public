05ee51a72b548907b089d4781c31290e24a2f0d4
Потапов Глеб Игоревич <gipotapov@edu.tversu.net>
2019-09-30 09:07:22 +0300

    helloworld

---
+++ main.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+void rangeBasedFor(std::vector<int> const& input) {
+	for (auto const& i : input) {
+		std::cout << i << " ";
+	}
+}
+void vectorSort(std::vector<int> vectorToSort) {
+	std::vector<int> sortedVector = vectorToSort;
+	std::sort(sortedVector.begin(), sortedVector.end());
+	rangeBasedFor(sortedVector);
+}
+
+std::vector<int> getVector() {
+	std::vector<int> newVector;
+	for (int i = 0; i < 5; i++) {
+		int curNumber;
+		std::cin >> curNumber;
+		newVector.push_back(curNumber);
+	}
+	return newVector;
+}
+int main()
+{
+	vectorSort(getVector());
+}
