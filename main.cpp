cf33901a3227713342b8f30b033ffb39856dd39b
Isignaturka <smanivkk@gmail.com>
2019-09-30 10:12:38 +0300

    yay

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+	std::vector<int>input = {};
+
+
+	int temp;
+	for (int i = 0; i < 5; i++) {
+		std::cin >> temp;
+		input.push_back(temp);
+	}
+
+	std::sort(input.begin(), input.end());
+	for (int i : input) {
+		std::cout << i;
+	}
+
+	return 0;
+}
