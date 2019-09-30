cfe6321215992ad09456250ef68fcbf725d7d02a
Olinegamer1 <pavel-lysenkov@yandex.ru>
2019-09-30 10:09:50 +0300

    gg

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
-#<unclude>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(){
-	std::cout << "Hello World!\n";
+
+
+
+
+int main()
+{   
+
+    std::vector<int> mas;
+    for (int i = 0; i < 5; i++) {
+        int elem;
+        std::cin >> elem;
+        mas.push_back(elem);
+    }
+    std::sort(std::begin(mas), std::end(mas));
+
+    for (int i : mas) {
+        std::cout << i << " ";
+    }
 }
